fb8b5c863eefa22d560d39a2fa7ba9a7f4b6855e
root <lce0077@naver.com>
2022-10-18 16:13:22 +0900

    commit

--- hanoi.c
+++ hanoi.c
@@ -1,17 +1,21 @@
 #include<stdio.h>
 
+int cnt = 0;
+
 void hanoi_tower(int n, char from, char tmp, char to)
 {
-    if (n == 1) printf("원판 1을 %c에서 %c으로 옮긴다.\n", from, to);
+    if (n == 1){ printf("원판 1을 %c에서 %c으로 옮긴다.\n", from, to); cnt += 1;}
+    
     else {
-        hanoi_tower(n - 1, from, to, tmp);
-        printf("원판 %d를 %c에서 %c으로 옮긴다.\n", n, from, to);
-        hanoi_tower(n - 1, tmp, from, to);
+        hanoi_tower(n - 1, from, to, tmp); 
+        printf("원판 %d를 %c에서 %c으로 옮긴다.\n", n, from, to); cnt += 1;
+        hanoi_tower(n - 1, tmp, from, to); 
     }
 }
 
 int main(void)
 {
     hanoi_tower(5, 'A', 'B', 'C');
+    printf("%d 번\n", cnt);
     return 0;
 }
